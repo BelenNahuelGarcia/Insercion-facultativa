034d204976ec09dbbd45fd14aee2e2c5ca4b17fa
lufly28 <florenciagira2007@gmail.com>
2024-09-10 15:14:46 -0300

    Enlaces discord

--- script4.js
+++ script4.js
@@ -7,17 +7,17 @@ form.addEventListener('submit', (event) => {
     // Aquí debes agregar la lógica para redireccionar al usuario al chat correspondiente
     // Por ejemplo:
     if (universidad === 'uba') {
-        window.location.href = 'https://discord.gg/s4d9UnPg';
+        window.location.href = 'https://discord.gg/DCCHakMMsh';
     } else if (universidad === 'utn') {
-        window.location.href = 'https://discord.gg/uzR9tPjy';
+        window.location.href = 'https://discord.gg/DHSUU5gdxQ';
     } else if (universidad === 'unlam') {
-        window.location.href = 'https://discord.gg/3QyH3jcV' ;
+        window.location.href = 'https://discord.gg/ydrvJkkKFv' ;
     } else if (universidad === 'unlp') {
-        window.location.href = 'https://discord.gg/T4t2pSXt' ;
+        window.location.href = 'https://discord.gg/k6VdHyQeyX' ;
     } else if (universidad === 'unsam') {
-        window.location.href = 'https://discord.gg/E4k5TQeS' ;
+        window.location.href = 'https://discord.gg/GJZHUZdUJ6' ;
     } else if (universidad === 'una')   {
-        window.location.href = 'https://discord.gg/F4VBUbNN';
+        window.location.href = 'https://discord.gg/r4djxKJrsN';
     }
 
   
